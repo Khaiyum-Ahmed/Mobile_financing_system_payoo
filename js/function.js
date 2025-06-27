7e5e8947d0d5d083dc78d89faea5a1ca8215acdc
Khaiyum-Ahmed <sayedshakin666@gmail.com>
2025-06-28 04:15:15 +0600

    add money form js complete with function

--- js/function.js
+++ js/function.js
@@ -1,1 +1,13 @@
 
+function getFieldValue (id){
+    const fieldValue = document.getElementById(id).innerText;
+    const fieldValueNumber = parseFloat(fieldValue);
+    return fieldValueNumber;
+}
+
+function getInputFieldValue (id){
+    const inputValue = document.getElementById(id).value;
+    const inputValueNumber = parseFloat(inputValue);
+    return inputValueNumber;
+}
+
--- js/payoo.js
+++ js/payoo.js
@@ -11,56 +11,90 @@
 
 
 
-// step 1: add an event handler to the add money button inside the form...
-document.getElementById('add-money-button').addEventListener('click', function () {
+// // step 1: add an event handler to the add money button inside the form...
 
-    // if using form tag then prevent page reload after form submit
-    // event.preventDefault();
+
+// document.getElementById('add-money-button').addEventListener('click', function () {
+
+//     // if using form tag then prevent page reload after form submit
+//     // event.preventDefault();
 
     
-    console.log('add money button clicked');
-    const bankSelect = document.getElementById('input-select-bank').value;
+//     console.log('add money button clicked');
+//     const bankSelect = document.getElementById('input-select-bank').value;
 
-    const bankAccountNumber = document.getElementById('input-bank-account').value;
+//     const bankAccountNumber = document.getElementById('input-bank-account').value;
 
-    // step 2: get money to be added to the account
+//     // step 2: get money to be added to the account
 
-    const ammountToAdd = document.getElementById('input-ammount-add').value;
-    const amountToAddNumber = parseFloat(ammountToAdd);
-    // console.log(amountToAddNumber)
-    // step 3: get the pin number provided
-    const inputPinNumber = document.getElementById('input-pin-number').value;
+//     const ammountToAdd = document.getElementById('input-ammount-add').value;
+//     const amountToAddNumber = parseFloat(ammountToAdd);
+//     // console.log(amountToAddNumber)
+//     // step 3: get the pin number provided
+//     const inputPinNumber = document.getElementById('input-pin-number').value;
 
 
-    // step 4: get the current balance
+//     // step 4: get the current balance
 
-        const balance = document.getElementById('account-balance').innerText;
-        const balanceNumber = parseFloat(balance);
-        // console.log(balanceNumber);
+//         const balance = document.getElementById('account-balance').innerText;
+//         const balanceNumber = parseFloat(balance);
+//         // console.log(balanceNumber);
 
-    // wrong way to verify 
+//     // wrong way to verify 
 
-    if (inputPinNumber === '1234') {
+//     if (inputPinNumber === '1234') {
 
         
 
-        // step 5: add addMoney input with balance
+//         // step 5: add addMoney input with balance
 
-        const newBalance = balanceNumber + amountToAddNumber;
-        // console.log(newBalance);
-        // console.log(typeof newBalance, typeof balanceNumber, typeof amountToAddNumber);
+//         const newBalance = balanceNumber + amountToAddNumber;
+//         // console.log(newBalance);
+//         // console.log(typeof newBalance, typeof balanceNumber, typeof amountToAddNumber);
 
-        // step 6: update the balance in the UI/DOM
+//         // step 6: update the balance in the UI/DOM
 
-        document.getElementById('account-balance').innerText =  newBalance;
+//         document.getElementById('account-balance').innerText =  newBalance;
+
+//     }
+//     else {
+//         alert('Invalid Input, please try again later')
+//     }
+// })
+
+
+document.getElementById('add-money-button').addEventListener ('click', function() {
+    const addMoney = getInputFieldValue('input-ammount-add');
+    // console.log(addMoney);
+   
+    const addMoneyPin = getInputFieldValue('input-pin-number');
+    // console.log(addMoneyPin);
+    
+
+    if(isNaN(addMoney) || isNaN(addMoneyPin)){
+        alert('Invalid Input Failed');
+        return;
+    }
+
+    if(addMoneyPin === 1234 ){
+        const balance = getFieldValue('account-balance');
+        const newBalance = balance + addMoney;
+        // console.log(newBalance)
+        document.getElementById('account-balance').innerText = newBalance;
+        //  addMoney = '';
+        //  addMoneyPin = '';
+        // add to transaction history...
+
+        const p = document.createElement('p');
+        p.innerText = `Added: ${addMoney} TK. New Balance: ${newBalance}`;
+
+        document.getElementById('transaction-container').appendChild(p);
 
     }
-    else {
-        alert('Invalid Input, please try again later')
+    else{
+        alert('Invalid Input, please try again later');
     }
-})
-
-
+});
 
 
 
@@ -72,34 +106,34 @@ document.getElementById('add-money-button').addEventListener('click', function (
 // CashOut or withdraw money section ...........
 
 
-document.getElementById('cashout-button').addEventListener('click', function(){
-    // console.log('withdraw money');
+// document.getElementById('cashout-button').addEventListener('click', function(){
+//     // console.log('withdraw money');
 
-    const agentNumber = document.getElementById('input-cashout-agent-number').value;
-    // console.log(agentNumber);
-    // agentNumber.value = '';
-    const cashOutAmount = document.getElementById('input-cashout-amount').value;
-    const cashOutAmountNumber = parseFloat(cashOutAmount);
-    // console.log(cashOutAmount);
-    // cashOutAmount.value = '';
-    const cashOutPinNumber = document.getElementById('input-cashout-pin-number').value;
-    // console.log(cashOutPinNumber);
-    // cashOutPinNumber.value = '';
+//     const agentNumber = document.getElementById('input-cashout-agent-number').value;
+//     // console.log(agentNumber);
+//     // agentNumber.value = '';
+//     const cashOutAmount = document.getElementById('input-cashout-amount').value;
+//     const cashOutAmountNumber = parseFloat(cashOutAmount);
+//     // console.log(cashOutAmount);
+//     // cashOutAmount.value = '';
+//     const cashOutPinNumber = document.getElementById('input-cashout-pin-number').value;
+//     // console.log(cashOutPinNumber);
+//     // cashOutPinNumber.value = '';
 
-    if(cashOutPinNumber === '1234'){
-        // console.log('you can withdraw money');
-        const accontBalance = document.getElementById('account-balance').innerText;
-        const accontBalanceNumber = parseFloat(accontBalance);
-        const newBalance = accontBalanceNumber - cashOutAmountNumber;
-        // console.log(newBalance);
+//     if(cashOutPinNumber === '1234'){
+//         // console.log('you can withdraw money');
+//         const accontBalance = document.getElementById('account-balance').innerText;
+//         const accontBalanceNumber = parseFloat(accontBalance);
+//         const newBalance = accontBalanceNumber - cashOutAmountNumber;
+//         // console.log(newBalance);
 
-        document.getElementById('account-balance').innerText = newBalance;
+//         document.getElementById('account-balance').innerText = newBalance;
 
-    }
-    else{
-        alert('Invalid Input, please try again later')
-    }
-})
+//     }
+//     else{
+//         alert('Invalid Input, please try again later')
+//     }
+// })
 
 
 
